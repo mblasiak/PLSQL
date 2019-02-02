e64f9ab76234554624002eeab811471709607e77
Milosz Blasiak <milblasiak@gmail.com>
2019-02-02 23:06:56 +0100

    Add unfinished task 3_4

---
+++ Lab_3/task_3_4.sql
@@ -0,0 +1,5 @@
+select * from czekoladki where koszt between 0.15 and 0.24 union select * from czekoladki where masa between 15 and 24 ;
+
+select * from czekoladki where koszt not between 0.25 and 0.35  intersect select * from czekoladki where masa between 25 and 35 ;
+
+
