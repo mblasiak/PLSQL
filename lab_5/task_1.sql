70bf5b41a45877ab4c46ae307b9d1ac39ccc7058
Milosz Blasiak <milblasiak@gmail.com>
2019-02-03 23:15:45 +0100

    Add task 1

---
+++ lab_5/task_1.sql
@@ -0,0 +1,12 @@
+select count(*) from czekoladki ;
+
+select count(*) from czekoladki where nadzienie is not Null;
+select count(nadzienie) from czekoladki;
+
+select sum(sztuk),idpudelka from zawartosc group by idpudelka order by 1 desc limit 1; 
+
+select sum(sztuk),idpudelka from zawartosc group by idpudelka order by 1;
+
+select sum(sztuk),idpudelka from zawartosc natural join czekoladki where orzechy is Null group by idpudelka order by 1 desc;
+
+select sum(sztuk),idpudelka from zawartosc natural join czekoladki where czekolada='mleczna' group by idpudelka order by 1 desc;
