d0758e115a3416d1ddc667e4587786ef832cc086
Milosz Blasiak <milblasiak@gmail.com>
2019-02-03 01:36:12 +0100

    Add TODO

--- Lab_3/task_3_4.sql
+++ Lab_3/task_3_4.sql
@@ -3,3 +3,4 @@ select * from czekoladki where koszt between 0.15 and 0.24 union select * from c
 select * from czekoladki where koszt not between 0.25 and 0.35  intersect select * from czekoladki where masa between 25 and 35 ;
 
 
+-- TODO Finish remaning exercises
