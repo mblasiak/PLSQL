c58e8322d58c7fa195a053034d1b07ba882e377f
Milosz Blasiak <milblasiak@gmail.com>
2019-02-04 13:16:16 +0100

    Rename and finish task 4

---
+++ lab_5/task_4.sql
@@ -0,0 +1,7 @@
+select count(*),idczekoladki from zawartosc group by idczekoladki order by 1 desc;
+
+select sum(sztuk),idpudelka from zawartosc natural join czekoladki where orzechy is Null group by idpudelka order by 1 desc;
+
+select count(idpudelka), idczekoladki from zawartosc full join czekoladki using(idczekoladki) group by idczekoladki order by 1 asc;
+
+select sum(sztuk),idpudelka from artykuly group by idpudelka order by 1 desc;
